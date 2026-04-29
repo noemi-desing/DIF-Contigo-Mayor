072075d3f1d249b1fac517d482b26fb547179c08
noemi-desing <noemi.rox011@gmail.com>
2026-04-29 11:46:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const socket = io();
+const asistente = window.speechSynthesis;
+
+function hablar(texto) {
+    if (asistente.speaking) {
+        asistente.cancel();
+    }
+    
+    const mensaje = new SpeechSynthesisUtterance(texto);
+    mensaje.lang = 'es-MX';
+    mensaje.rate = 0.8; // Velocidad clara para adultos mayores
+    asistente.speak(mensaje);
+}
+
+document.getElementById('btn-sos').onclick = () => {
+    hablar("Alerta de emergencia activada. Estamos avisando a tu familia ahora mismo.");
+    socket.emit('sos-activado', {
+        id: "Usuario-Jalisco",
+        hora: new Date().toLocaleTimeString()
+    });
+};
+
+document.getElementById('btn-acompanamiento').onclick = () => {
+    hablar("Iniciando modo acompañamiento diario. ¿Deseas escuchar tus recordatorios de hoy?");
+};
+
+document.getElementById('btn-asesoria').onclick = () => {
+    hablar("Bienvenido a asistencia social. Tienes una cita pendiente para tu tarjeta de transporte.");
+};
+
+window.onload = () => {
+    setTimeout(() => {
+        hablar("Bienvenido a tu aplicación de apoyo DIF Contigo Mayor.");
+    }, 500);
+};
